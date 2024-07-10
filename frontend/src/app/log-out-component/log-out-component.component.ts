9669e89bd5eb78eadba70b6337538833669f9b16
hanine <haninebenamor45@gmail.com>
2024-07-10 13:31:38 +0100

    commiting

---
+++ frontend/src/app/log-out-component/log-out-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-log-out-component',
+  templateUrl: './log-out-component.component.html',
+  styleUrl: './log-out-component.component.css'
+})
+export class LogOutComponentComponent {
+
+}
---
+++ frontend/src/app/role-list-component/role-list-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-role-list-component',
+  templateUrl: './role-list-component.component.html',
+  styleUrl: './role-list-component.component.css'
+})
+export class RoleListComponentComponent {
+
+}
